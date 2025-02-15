45564b40e8b6276d7c6b5692856e65df50e35122
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2025-02-15 08:34:49 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -13,13 +13,7 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "https://your-frontend.vercel.app",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
